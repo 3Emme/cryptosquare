94ba6ecdcbb343d8c31951752cabb20161c8a5d4
3Emme <emmettbuentiempo@gmail.com>
2020-08-20 17:53:17 -0700

    pass spec 5 test.
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,19 +19,23 @@ function cryptosquare (initialText){
 function rowsColumns(length,arrayText) {  
   let square = Math.sqrt(length)
   console.log(square)
-  console.log( columns = Math.ceil(square)) //ARRAY LENGTH
-  console.log( rows = Math.floor(square)) // HOW MANY ARRAYS
+  console.log( columns = Math.floor(square)) //ARRAY LENGTH
+  console.log( rows = Math.ceil(square)) // HOW MANY ARRAYS
   let = cryptoArray = []
-  for (let index = 0; index < rows; index +=1)
-    for (let index = 0; index < arrayText.length; index +=columns)   
-      if (arrayText[index] !== undefined) {
-        cryptoArray.push(arrayText[index])
+  let indexMod = 0
+  for (let index = 0; index < columns; index +=1) {
+    console.log("indexMod=",indexMod)
+    console.log("LOOP 1 ROUND",index)
+    for ( let index2 = 0 + indexMod; index2 < arrayText.length; index2 +=columns) {  
+      console.log("LOOP 2 ROUND",index)
+      if (arrayText[index2] !== undefined) {
+        cryptoArray.push(arrayText[index2])      
       } 
-  console.log(cryptoArray)            
-}
+    } indexMod += 1
+  }
+  console.log(cryptoArray)
+} 
 
-// let MASTERGRID = [0, 1, 2] 
-// for ROWS { build an array with length COLUMNS}
 
 
 
